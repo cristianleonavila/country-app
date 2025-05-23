f096423fa4b4f5327c696a7470a4791bd5fa07c8
Cristian Leon <cristian.leon.avila@gmail.com>
2025-05-23 18:29:49 -0500

    Country App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes')
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
